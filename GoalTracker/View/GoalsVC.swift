ce43fd26ed16e853ffaeda454c95b64f4a85f0f5
ssh <kathishivaram@gmail.com>
2019-07-11 21:27:09 +0530

    setup userinterface

---
+++ GoalTracker/View/GoalsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalTracker
+//
+//  Created by apple on 7/11/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
