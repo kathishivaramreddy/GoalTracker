8f73ed73afe70036d5a098c8ad78ed1fb07d2e26
ssh <kathishivaram@gmail.com>
2019-07-12 12:12:10 +0530

    add create goal view controller

---
+++ GoalTracker/Controller/GoalsAddVCViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GoalsAddVCViewController.swift
+//  GoalTracker
+//
+//  Created by apple on 7/12/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class GoalsAddVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
--- GoalTracker/View/GoalsVC.swift
+++ GoalTracker/View/GoalsVC.swift
@@ -16,23 +16,37 @@ class GoalsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
+    
+    
 
-
+    @IBAction func addGoalButtonPressed(_ sender: UIButton) {
+        
+        guard let addGoalVC = storyboard?.instantiateViewController(withIdentifier: "GoalsAddVC") else { return }
+        present(addGoalVC, animated: true, completion: nil)
+    }
+    
 }
 
 
 extension GoalsVC : UITableViewDelegate , UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCellTableViewCell else {
             return UITableViewCell()
         }
+        cell.configureGoalCell(description: "Apply for swift jobs", type: .longTerm, progress: 3)
         return cell
     }
     
