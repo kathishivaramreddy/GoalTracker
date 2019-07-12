7955c68b56a38e5cd91491698cb53f9be85e4197
ssh <kathishivaram@gmail.com>
2019-07-12 16:51:47 +0530

    store goal to core data

--- GoalTracker/Controller/CreateGoalVC.swift
+++ GoalTracker/Controller/CreateGoalVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class CreateGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -23,18 +26,50 @@ class CreateGoalVC: UIViewController, UITextFieldDelegate {
     var goalType: GoalType!
     
     func initData(goal: String, type: GoalType) {
-        
+        self.goalDescription = goal
+        self.goalType = type
     }
     
     
     @IBAction func createGoalButtonPressed(_ sender: UIButton) {
         
+        print("in create goal")
         
+        if goalPointTextField.text! != "" {
+            save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
         
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func save(completion: (_ finished:Bool) -> ()) {
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletion = Int32(goalPointTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+           try managedContext.save()
+            completion(true)
+        }catch {
+            debugPrint("\(error.localizedDescription)")
+            completion(false)
+        }
+        
+        
     }
     
 }
